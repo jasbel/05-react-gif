5eb6dda4527e457c541a54b47ce0c8e43eff02f8
Ajar PC <asbel.elmec@gmail.com>
2020-12-08 18:42:15 -0400

    ADD last code

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
 const GifGrid = ({ category }) => {
+
+    //usando la api de giphy
+    const getGifs = async () => {
+        const url = 'https://api.giphy.com/v1/gifs/search?q=rick+and+morty&limit=10&api_key=XyiNnwGMpqYVMgHjojGHlfn51EuC5Wqn';
+
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        console.log(data);
+
+    }
+
+    getGifs();
+
     return (
         <div>
             <h3 > { category } </h3>
